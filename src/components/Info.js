4ed4853fb1af88c42043e7fd998ccd4d5bd13179
Olya2125 <139589012+Olya2125@users.noreply.github.com>
2023-10-29 22:13:38 +0300

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Photo from "../photo.jpg"
+
+
+const Info = () => {
+    return (
+        <div className="info">
+
+<img src={Photo} alt="photo"/>
+    
+        </div>
+    )
+}
+
+export default Info;
